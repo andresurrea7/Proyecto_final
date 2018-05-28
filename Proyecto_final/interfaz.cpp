c505cb35fb37933c86ffc2d1fed0622843b3e48f
Andres <andres.urrea@udea.edu.co>
2018-05-28 15:56:38 -0500

    commit menu

--- Proyecto_final/interfaz.cpp
+++ Proyecto_final/interfaz.cpp
@@ -1,14 +1,60 @@
 #include "interfaz.h"
 #include "ui_interfaz.h"
+#include <QMediaPlayer>
+#include <QFileDialog>
+
 
 interfaz::interfaz(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::interfaz)
 {
     ui->setupUi(this);
+    mMediaPlayer = new QMediaPlayer(this);
+
 }
 
 interfaz::~interfaz()
 {
     delete ui;
+}
+
+
+
+void interfaz::on_amigo_clicked()
+{
+
+}
+
+
+void interfaz::on_puntaje_clicked()
+{
+
+}
+
+void interfaz::on_bye_clicked()
+{
+
+}
+
+void interfaz::on_nuevo_clicked()
+{
+
+}
+
+
+
+void interfaz::on_pushButton_3_clicked()
+{
+
+}
+
+void interfaz::on_pushButton_clicked()
+{
+    QString filename= QFileDialog::getOpenFileName(this,"Abrir");
+    if(filename.isEmpty()){
+        return;
+    }
+    mMediaPlayer->setMedia(QUrl::fromLocalFile(filename));
+    //on_play_clicked();
+    mMediaPlayer->play();
 }
--- Proyecto_final/interfaz.h
+++ Proyecto_final/interfaz.h
@@ -3,20 +3,39 @@
 
 #include <QMainWindow>
 
+
 namespace Ui {
 class interfaz;
 }
-
+class QMediaPlayer;
 class interfaz : public QMainWindow
 {
     Q_OBJECT
 
 public:
     explicit interfaz(QWidget *parent = 0);
+
     ~interfaz();
+
+
+private slots:
+
+
+    void on_amigo_clicked();
+
+    void on_puntaje_clicked();
+
+    void on_bye_clicked();
+
+    void on_nuevo_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_clicked();
 
 private:
     Ui::interfaz *ui;
+    QMediaPlayer *mMediaPlayer;
 };
 
 #endif // INTERFAZ_H
